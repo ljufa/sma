d43563c8b055307b2809a6ca285f159e19dda14a
Dragan <dragan.ljubojevic@gmail.com>
2022-11-08 00:26:21 +0100

    initial import

--- backend/src/main/kotlin/com/github/ljufa/sma/backend/api/SecurityConfig.kt
+++ backend/src/main/kotlin/com/github/ljufa/sma/backend/api/SecurityConfig.kt
@@ -20,11 +20,7 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 @EnableReactiveMethodSecurity
 class SecurityConfig {
 
-    @Value("\${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-    private lateinit var issuer: String
-
     @Bean
-//    @Profile("local")
     @Throws(Exception::class)
     fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
         return http
@@ -32,41 +28,20 @@ class SecurityConfig {
             .build()
     }
 
-//    @Bean
-//    @Throws(Exception::class)
-//    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
-//        return http
-//            .authorizeExchange()
-//            .anyExchange().authenticated()
-//            .and().cors()
-//            .and().oauth2ResourceServer()
-//            .jwt()
-//            .and()
-//            .and()
-//            .build()
-//    }
-
     @Bean
-    fun jwtDecoder(): ReactiveJwtDecoder {
-        return ReactiveJwtDecoders.fromOidcIssuerLocation(issuer) as NimbusReactiveJwtDecoder
-    }
-
-    @Bean
-//    @Profile("local")
     fun corsConfiguration(): CorsConfigurationSource {
         val corsConfig = CorsConfiguration()
         corsConfig.applyPermitDefaultValues()
         corsConfig.allowCredentials = true
         corsConfig.addAllowedMethod("GET")
         corsConfig.addAllowedMethod("POST")
-        corsConfig.allowedOrigins = listOf("http://localhost:8000")
+        corsConfig.allowedOrigins = listOf("http://localhost:8000", "https://sma.dlj.freemyip.com")
         val source = UrlBasedCorsConfigurationSource()
         source.registerCorsConfiguration("/**", corsConfig)
         return source
     }
 
     @Bean
-//    @Profile("local")
     fun corsWebFilter(): CorsWebFilter? {
         return CorsWebFilter(corsConfiguration())
     }
